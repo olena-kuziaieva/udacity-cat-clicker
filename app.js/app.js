e482acbcb94ae1f00fa168d0b9ea6c8a7bd70f41
Olena Kuziaieva <kusaeva@gmail.com>
2019-09-08 13:58:15 +0200

    Delete unnecessary folder

--- app.js/app.js
+++
@@ -1,8 +0,0 @@
-var numOfMadeClicks = 0;
-
-var elem = document.getElementById("catImg");
-elem.addEventListener("click", function() {
-    numOfMadeClicks += 1;
-    elem.innerHTML = numOfMadeClicks;
-}, false);
-
