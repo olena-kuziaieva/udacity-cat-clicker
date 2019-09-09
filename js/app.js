93f71594fc4dccd2ec680556b44d10a45f3644b0
Olena Kuziaieva <kusaeva@gmail.com>
2019-09-09 23:31:21 +0200

    Add handling of two independent click events

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-var numOfMadeClicks = 0;
+var catFirst = {name: "Fluffy", clicks: 0};
+var catSecond = {name: "Muffy", clicks: 0};
 
-var elem = document.getElementById("catImg");
-elem.addEventListener("click", function() {
-    numOfMadeClicks += 1;
-    document.getElementsByClassName("numOfClicks")[0].innerHTML = numOfMadeClicks;
+document.getElementById("catName1").innerHTML = "Cat name: " + catFirst.name;
+document.getElementById("catName2").innerHTML = "Cat name: " + catSecond.name;
+
+var cat1Element = document.getElementById("cat1Img");
+cat1Element.addEventListener("click", function (e) {
+    catFirst.clicks += 1;
+    e.currentTarget.getElementsByClassName("numOfClicks")[0].innerHTML = catFirst.clicks;
+}, false);
+
+var cat2Element = document.getElementById("cat2Img");
+cat2Element.addEventListener("click", function (e) {
+    catSecond.clicks += 1;
+    e.currentTarget.getElementsByClassName("numOfClicks")[0].innerHTML = catSecond.clicks;
 }, false);
 
